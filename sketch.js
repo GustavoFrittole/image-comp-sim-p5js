1bc52b0220e7bc8f2528f8eabea916bcf15bd748
GustavoFrittole <gabriele.callegaro@gmail.com>
2025-04-05 00:58:33 +0200

    testing functionalities

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
-function setup() {
-  createCanvas(400, 400);
+img = null
+
+function preload()
+{
+  img = loadImage('resources/cute_image.jpg');
 }
 
-function draw() {
-  background(220);
+function setup() 
+{
+  createCanvas(img.width, img.height);
+}
+
+function draw()
+{
+  background(10);
+  img.loadPixels();
+  for(y=0; y<img.height; ++y)
+    for(x=0; x<img.width * 4; x += 4)
+    {
+      img.pixels[x + img.width * y * 4] = (++img.pixels[x + img.width * y * 4]) % 255;
+    }
+  console.log(frameCount);
+  img.updatePixels();
+  image(img, 0, 0);
 }
